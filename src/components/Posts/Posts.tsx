2478d594c9279d3543d40e8c48429290e33fb2df
Shima-Oh <shimasound04@gmail.com>
2021-11-27 20:59:57 +0900

    feat(posts.tsx): ブログの内容のプレビューを表示

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,19 +1,37 @@
-import { HStack, Box, Heading, Tag, Link, Divider } from '@chakra-ui/react';
+import {
+  HStack,
+  Stack,
+  Box,
+  Heading,
+  Tag,
+  Link,
+  Divider,
+  Text,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { VFC } from 'react';
 import { blog } from '../../types/responseDataType';
 import DateFormat from '../../util/DateFormat';
+import styled from '@emotion/styled';
 
 type Props = {
   blogs: blog[];
 };
 
+const BodyPreviwe = styled.p`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 const Article: VFC<Props> = ({ blogs }) => {
   return (
     <Box>
       {blogs.map((blog) => (
-        <>
-          <Box key={blog.id}>
+        <Box key={blog.id} mb={8}>
+          <Stack>
             <HStack>
               <Box as="time" dateTime={blog.createdAt}>
                 {DateFormat(blog.createdAt)}
@@ -25,9 +43,17 @@ const Article: VFC<Props> = ({ blogs }) => {
                 {blog.title}
               </Link>
             </Heading>
-          </Box>
+            <BodyPreviwe>
+              {blog.body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')}
+            </BodyPreviwe>
+            <Text fontWeight="bold">
+              <Link as={NextLink} href={`/blog/${blog.id}`}>
+                Read More
+              </Link>
+            </Text>
+          </Stack>
           <Divider mt={4} />
-        </>
+        </Box>
       ))}
     </Box>
   );
